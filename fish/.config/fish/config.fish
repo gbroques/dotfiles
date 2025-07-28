a01f128c437d78263781d23ce24b982b4eebf694
G Roques <groques360@gmail.com>
2025-07-27 22:35:15 -0500

    (fish): Fix fisher plugin manager

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -46,7 +46,19 @@ abbr -a -- fcdh 'cd (fd --type d --hidden | fzf)' # fuzzy cd hidden
 # -------------------------------------------------------------------
 
 # Fisher
+# ------
+# Install Fisher to ~/.config/fish/fisher/ for easier ignoring in version control.
+#
+# See:
+# https://github.com/jorgebucaran/fisher/issues/640#issuecomment-1878499811
 set fisher_path $__fish_config_dir/fisher
+
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
+
+for file in $fisher_path/conf.d/*.fish
+    source $file
+end
 
 # Zoxide
 if command --query zoxide
