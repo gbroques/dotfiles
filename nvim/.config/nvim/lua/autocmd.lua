f4436570f35470ac8894e3629bc0b174fb335e45
G Roques <groques360@gmail.com>
2025-07-31 16:43:44 -0500

    (nvim): Add groups to autocommands so they aren't execute multiple times

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -1,44 +1,56 @@
 -- Autocommands
 -- :help autocommands
+-- :help events
+
 -- TJ DeVries - [Demo] Lua Autocmd Patterns
 -- https://www.youtube.com/watch?v=HR1dKKrOmDs
--- :help events
+
 -- TJ DeVries - [Demo] Lua Augroups - Why And How To Use?
 -- https://www.youtube.com/watch?v=F6GNPOXpfwU
+
 -- See also:
 -- https://itmecho.com/blog/neovim-lua-hooks-with-user-events
 
--- Highlight yanked selection.
--- https://neovim.io/doc/user/lua.html#vim.hl
-local handle_highlight = function()
-  vim.hl.on_yank({ higroup = 'IncSearch' })
-end
-vim.api.nvim_create_autocmd('TextYankPost', { pattern = '*', callback = handle_highlight })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight yanked selection',
+  -- https://neovim.io/doc/user/lua.html#vim.hl
+  group = vim.api.nvim_create_augroup('highlight_yanked_selection', { clear = true }),
+  pattern = '*',
+  callback = function()
+    vim.hl.on_yank({ higroup = 'IncSearch' })
+  end
+})
 
--- Automatically make the help window the only one on the screen.
--- https://vi.stackexchange.com/a/39697
 vim.api.nvim_create_autocmd('BufWinEnter', {
+  desc = 'Automatically make the help window the only one on the screen.',
+  -- https://vi.stackexchange.com/a/39697
   group = vim.api.nvim_create_augroup('maximize_help', { clear = true }),
-  pattern = { '*.txt' },
+  pattern = '*.txt',
   callback = function()
     if vim.o.filetype == 'help' then vim.cmd.wincmd('o') end
   end
 })
 
--- Automatically enter INSERT mode upon opening a terminal.
--- https://vi.stackexchange.com/questions/3670/how-to-enter-insert-mode-when-entering-neovim-terminal-pane
--- https://github.com/nvim-neotest/neotest/issues/2#issuecomment-1149532666
 vim.api.nvim_create_autocmd('TermOpen', {
+  desc = 'Automatically enter INSERT mode upon opening a terminal.',
+  -- https://vi.stackexchange.com/questions/3670/how-to-enter-insert-mode-when-entering-neovim-terminal-pane
+  -- https://github.com/nvim-neotest/neotest/issues/2#issuecomment-1149532666
+  group = vim.api.nvim_create_augroup('enter_insert_mode_upon_opening_term', { clear = true }),
+  pattern = '*',
   callback = function()
     if vim.startswith(vim.api.nvim_buf_get_name(0), 'term://') then
       vim.cmd('startinsert')
     end
   end
 })
--- Automatically close terminal buffer when process exits successfully (Ctrl + d),
--- or when using the exit command in fish.
--- https://fishshell.com/docs/current/cmds/exit.html
+
 vim.api.nvim_create_autocmd('TermClose', {
+  desc = 'Automatically close terminal buffer when process exits',
+  -- when the process exits successfully (pressing Ctrl + d),
+  -- or when using the exit command in fish. See:
+  -- https://fishshell.com/docs/current/cmds/exit.html
+  group = vim.api.nvim_create_augroup('close_terminal_buffer_when_process_exits', { clear = true }),
+  pattern = '*',
   callback = function()
     local status = vim.v.event.status
     if status == 0 or status == 255 then
