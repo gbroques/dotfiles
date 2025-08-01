49faad6b47ede7d59c990d5282c204159fdbf152
G Roques <groques360@gmail.com>
2025-07-31 20:58:00 -0500

    (nvim): Add neovide configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,6 +2,7 @@ require('globals')
 require('autocmd')
 require('plugin-manager')
 require('keymaps')
+require('neovide')
 require('options')
 require('diagnostics')
 
---
+++ nvim/.config/nvim/lua/neovide.lua
@@ -0,0 +1,3 @@
+-- Neovide configuration
+-- https://neovide.dev/configuration.html
+vim.g.neovide_cursor_trail_size = 0.0
