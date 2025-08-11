b5f000539e3e14c61d3a0aae579e43f7dc343ae8
G Roques <groques360@gmail.com>
2025-08-10 19:39:12 -0500

    (nvim): Add TODO comment to replace nvim-spectre with grug-far

--- nvim/.config/nvim/lua/plugins/global-find-and-replace.lua
+++ nvim/.config/nvim/lua/plugins/global-find-and-replace.lua
@@ -1,6 +1,7 @@
 -- Project-wide find and replace
 return {
   {
+    -- TODO: Replace with https://github.com/MagicDuck/grug-far.nvim
     'nvim-pack/nvim-spectre',
     commit = '72f56f7585903cd7bf92c665351aa585e150af0f',
     cmd = 'Spectre',
