5ce349bdc749428b26381902f4846d4cbcc0e454
G Roques <groques360@gmail.com>
2025-10-28 20:11:48 -0500

    (nvim): Open Amazon Q chat window to the right (instead of default left)

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -5,6 +5,13 @@ return {
     config = function()
       require('amazonq').setup({
         ssoStartUrl = 'https://charter-cloud-connect.awsapps.com/start',
+        -- Open chat window on right (instead of default left)
+        on_chat_open = function()
+          vim.cmd [[
+            vertical botright split
+            set wrap breakindent nonumber norelativenumber nolist
+          ]]
+        end,
         -- Uncomment the below line to debug plugin:
         -- debug = true
       })
