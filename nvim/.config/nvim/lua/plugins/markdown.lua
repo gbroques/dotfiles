1351e308fc35b20645479538591581f6caffb93b
G Roques <groques360@gmail.com>
2026-04-07 11:08:11 -0500

    (nvim): Add selimacerbas/markdown-preview.nvim for browser markdown preview

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -2,5 +2,18 @@ return {
   {
     'bullets-vim/bullets.vim',
     tag = '2.0.0'
-  }
+  },
+  {
+    'selimacerbas/markdown-preview.nvim',
+    tag = 'v1.5.3',
+    ft = { 'markdown', 'mermaid' },
+    dependencies = { 'selimacerbas/live-server.nvim' },
+    config = function()
+      require('markdown_preview').setup()
+    end,
+    keys = {
+      { '<leader>mp', '<cmd>MarkdownPreview<cr>', desc = 'Markdown: Start preview' },
+      { '<leader>mP', '<cmd>MarkdownPreviewStop<cr>', desc = 'Markdown: Stop preview' },
+    },
+  },
 }
